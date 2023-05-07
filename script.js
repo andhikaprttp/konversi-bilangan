0fdcc3fe5fcc7976bac364022f938cc04aa7f928
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-07 19:17:25 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-		/*
+/*
 		
 		Masalah : waktu untuk menghitung 
 		Solusi  : membuat otomasisasi perhitungan untuk konversi sistem bilangan.
@@ -15,60 +15,67 @@
 		
 		
 		*/
-		
-		
-		// Konversi desimal ke bilangan lain
-		function convertDecimal() {
-			let desimal = document.getElementById("desimal").value;
 
-			// Konversi ke biner
-			document.getElementById("biner").value = parseInt(desimal).toString(2);
+// Konversi desimal ke bilangan lain
+function convertDecimal() {
+  let desimal = document.getElementById("desimal").value;
 
-			// Konversi ke oktal
-			document.getElementById("oktal").value = parseInt(desimal).toString(8);
+  // Konversi ke biner
+  document.getElementById("biner").value = parseInt(desimal).toString(2);
 
-			// Konversi ke heksadesimal
-			document.getElementById("hexadesimal").value = parseInt(desimal).toString(16);
-		}
+  // Konversi ke oktal
+  document.getElementById("oktal").value = parseInt(desimal).toString(8);
 
-		// Konversi biner ke bilangan lain
-		function convertBinary() {
-			let biner = document.getElementById("biner").value;
+  // Konversi ke heksadesimal
+  document.getElementById("hexadesimal").value = parseInt(desimal).toString(16);
+}
 
-			// Konversi ke desimal
-			document.getElementById("desimal").value = parseInt(biner, 2);
+// Konversi biner ke bilangan lain
+function convertBinary() {
+  let biner = document.getElementById("biner").value;
 
-			// Konversi ke oktal
-			document.getElementById("oktal").value = parseInt(biner, 2).toString(8);
+  // Konversi ke desimal
+  document.getElementById("desimal").value = parseInt(biner, 2);
 
-			// Konversi ke heksadesimal
-			document.getElementById("hexadesimal").value = parseInt(biner, 2).toString(16);
-		}
+  // Konversi ke oktal
+  document.getElementById("oktal").value = parseInt(biner, 2).toString(8);
 
-		// Konversi oktal ke bilangan lain
-		function convertOctal() {
-			let oktal = document.getElementById("oktal").value;
+  // Konversi ke heksadesimal
+  document.getElementById("hexadesimal").value = parseInt(biner, 2).toString(
+    16
+  );
+}
 
-			// Konversi ke desimal
-			document.getElementById("desimal").value = parseInt(oktal, 8);
+// Konversi oktal ke bilangan lain
+function convertOctal() {
+  let oktal = document.getElementById("oktal").value;
 
-			// Konversi ke biner
-			document.getElementById("binary").value = parseInt(oktal, 8).toString(2);
+  // Konversi ke desimal
+  document.getElementById("desimal").value = parseInt(oktal, 8);
 
-			// Konversi ke heksadesimal
-			document.getElementById("hexadesimal").value = parseInt(oktal, 8).toString(16);
-		}
+  // Konversi ke biner
+  document.getElementById("binary").value = parseInt(oktal, 8).toString(2);
 
-		// Konversi heksadesimal ke bilangan lain
-		function convertHexadecimal() {
-			let hexadesimal = document.getElementById("hexadesimal").value;
+  // Konversi ke heksadesimal
+  document.getElementById("hexadesimal").value = parseInt(oktal, 8).toString(
+    16
+  );
+}
 
-			// Konversi ke desimal
-			document.getElementById("desimal").value = parseInt(hexadesimal, 16);
+// Konversi heksadesimal ke bilangan lain
+function convertHexadecimal() {
+  let hexadesimal = document.getElementById("hexadesimal").value;
 
-			// Konversi ke biner
-			document.getElementById("biner").value = parseInt(hexadesimal, 16).toString(2);
+  // Konversi ke desimal
+  document.getElementById("desimal").value = parseInt(hexadesimal, 16);
 
-			// Konversi ke oktal
-			document.getElementById("oktal").value = parseInt(hexadesimal, 16).toString(8);
-		}
+  // Konversi ke biner
+  document.getElementById("biner").value = parseInt(hexadesimal, 16).toString(
+    2
+  );
+
+  // Konversi ke oktal
+  document.getElementById("oktal").value = parseInt(hexadesimal, 16).toString(
+    8
+  );
+}
