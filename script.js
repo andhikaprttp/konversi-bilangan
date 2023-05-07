33b7aa12654797e875dd1afc37150259cce28379
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-07 14:32:44 +0700

    update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
 			document.getElementById("oktal").value = parseInt(biner, 2).toString(8);
 
 			// Konversi ke heksadesimal
-			document.getElementById("hexadesimal").value = parseInt(binary, 2).toString(16);
+			document.getElementById("hexadesimal").value = parseInt(biner, 2).toString(16);
 		}
 
 		// Konversi oktal ke bilangan lain
