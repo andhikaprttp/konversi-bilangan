a9985be8045a7146441951310b5aa647605eab88
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-11-13 10:31:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,36 @@
-/*
-		
-		Masalah : waktu untuk menghitung 
-		Solusi  : membuat otomasisasi perhitungan untuk konversi sistem bilangan.
-		
-		implementasi :
-		======================================
-		Sistem Bilangan ada 4 yaitu Desimal , Biner , Hexadesimal dan Oktal.
-		======================================
-		Desimal bilangan basis 10
-		Biner bilangan basis 2 ( 0 dan 1 )
-		Hexadesimal bilangan basis 16
-		Oktal bilangan basis 8
-		======================================
-	
-		
-		*/
-
-// Konversi desimal ke bilangan lain
+// Function untuk konversi desimal ke bilangan lain
 function convertDecimal() {
-  let desimal = document.getElementById("desimal").value;
+  let desimal = parseInt(document.getElementById("desimal").value, 10);
 
-  // Konversi ke biner
-  document.getElementById("biner").value = parseInt(desimal).toString(2);
-
-  // Konversi ke oktal
-  document.getElementById("oktal").value = parseInt(desimal).toString(8);
-
-  // Konversi ke heksadesimal
-  document.getElementById("hexadesimal").value = parseInt(desimal).toString(16);
+  document.getElementById("biner").value = desimal.toString(2);
+  document.getElementById("oktal").value = desimal.toString(8);
+  document.getElementById("hexadesimal").value = desimal.toString(16);
 }
 
-// Konversi biner ke bilangan lain
+// Function untuk konversi biner ke bilangan lain
 function convertBinary() {
   let biner = document.getElementById("biner").value;
 
-  // Konversi ke desimal
   document.getElementById("desimal").value = parseInt(biner, 2);
-
-  // Konversi ke oktal
   document.getElementById("oktal").value = parseInt(biner, 2).toString(8);
-
-  // Konversi ke heksadesimal
-  document.getElementById("hexadesimal").value = parseInt(biner, 2).toString(
-    16
-  );
+  document.getElementById("hexadesimal").value = parseInt(biner, 2).toString(16);
 }
 
-// Konversi oktal ke bilangan lain
+// Function untuk konversi oktal ke bilangan lain
 function convertOctal() {
   let oktal = document.getElementById("oktal").value;
 
-  // Konversi ke desimal
   document.getElementById("desimal").value = parseInt(oktal, 8);
-
-  // Konversi ke biner
-  document.getElementById("binary").value = parseInt(oktal, 8).toString(2);
-
-  // Konversi ke heksadesimal
-  document.getElementById("hexadesimal").value = parseInt(oktal, 8).toString(
-    16
-  );
+  document.getElementById("biner").value = parseInt(oktal, 8).toString(2);
+  document.getElementById("hexadesimal").value = parseInt(oktal, 8).toString(16);
 }
 
-// Konversi heksadesimal ke bilangan lain
+// Function untuk konversi heksadesimal ke bilangan lain
 function convertHexadecimal() {
   let hexadesimal = document.getElementById("hexadesimal").value;
 
-  // Konversi ke desimal
   document.getElementById("desimal").value = parseInt(hexadesimal, 16);
-
-  // Konversi ke biner
-  document.getElementById("biner").value = parseInt(hexadesimal, 16).toString(
-    2
-  );
-
-  // Konversi ke oktal
-  document.getElementById("oktal").value = parseInt(hexadesimal, 16).toString(
-    8
-  );
-}
-
-// SMK RADEN PAKU 2023
+  document.getElementById("biner").value = parseInt(hexadesimal, 16).toString(2);
+  document.getElementById("oktal").value = parseInt(hexadesimal, 16).toString(8);
+						    }
+						    
